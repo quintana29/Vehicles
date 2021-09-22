de06bb2aec6839932b8563578791feeaad7bfff1
JHONATAN QUINTANA TORO <jhonatanquintana266873@correo.itm.edu.co>
2021-09-22 11:43:21 -0500

    Add vehicle type

---
+++ Vehicles.API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //Creando conexion a la base de datos
+        public DataContext(DbContextOptions<DataContext> options): base (options)
+        {
+        }
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+        //Agregar indice a la tabla
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        //Create table in DB
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo de vehiculo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de {1} carácteres." )]
+        [Required(ErrorMessage = "El campo {0} es obligatrio.")]
+        public string Description { get; set; }
+    }
+}
